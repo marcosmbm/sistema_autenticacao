692885c36c18f401d701fdd64783fc5509633a5e
Marcos Barbosa <marmbmti@gmail.com>
2022-01-12 08:19:44 -0300

    criando contexto

---
+++ client/src/context/auth.js
@@ -0,0 +1,84 @@
+import {useState,createContext,useEffect} from 'react';
+
+import api from '../services/api';
+
+export const AuthContext = createContext({});
+
+function AuthProvider({children}){
+    ///Meu objeto de usuario
+    const [user,setUser] = useState(null);
+
+    useEffect(() => {
+        function loadStorage(){
+            const storageUser = localStorage.getItem('Users');
+
+            if(storageUser){
+                setUser(JSON.parse(storageUser));
+            }
+        }
+        loadStorage();
+    },[]);
+
+    ///Função responsável para cadastrar
+    function signUp(name,email,password){
+        api.post('/register',{
+            name: name,
+            email: email,
+            password: password
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    ///Função responsável para logar
+    function signIn(email,password){
+        api.post('/login',{
+            email: email,
+            password: password
+        })
+        .then((response) => {
+            console.log(response.data);
+
+            let data = {
+                nome: response.data.nome,
+                email: response.data.email
+            }
+
+            setUser(data);
+            storageUser(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    function storageUser(data){
+        localStorage.setItem('Users', JSON.stringify(data));
+    }
+
+    function signOut(){
+        localStorage.removeItem('Users');
+        setUser(null);
+    }
+
+
+
+    return(
+        <AuthContext.Provider
+            value={{
+                user,
+                signUp,
+                signIn,
+                signOut
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
